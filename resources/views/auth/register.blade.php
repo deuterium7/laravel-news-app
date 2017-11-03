7405f6d30e2edac3fd34af62387661205d779309
deuterium7 <post0778@gmail.com>
2017-11-03 14:18:16 +0200

    Пофиксил имя пользователя

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">@lang('catalog.name')</label>
+                            <label for="name" class="col-md-4 control-label">@lang('catalog.nameUser')</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
