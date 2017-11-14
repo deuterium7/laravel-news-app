6b8e5b7ddaa76e52842a8185bee98a9de2af818c
deuterium7 <post0778@gmail.com>
2017-11-14 13:05:40 +0200

    Обновил константы

--- config/constants.php
+++ config/constants.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'ARTICLES_SEED'   => 15,
+    'ARTICLES_SEED'   => 150000,
     'CATEGORIES_SEED' => 10,
-    'COMMENTS_SEED'   => 500,
-    'USERS_SEED'      => 30,
+    'COMMENTS_SEED'   => 5000000,
+    'USERS_SEED'      => 300000,
 
 ];
