0643df12befd75864cf44f3bc97a0650e5e32c80
deuterium7 <post0778@gmail.com>
2017-11-24 14:25:10 +0200

    Пофиксил поле <image>

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -3,8 +3,6 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Article::class, function (Faker $faker) {
-    static $image;
-
     return [
         'category_id' => function () {
             return factory(\App\Models\Category::class)->create()->id;
@@ -13,7 +11,7 @@ $factory->define(\App\Models\Article::class, function (Faker $faker) {
             return factory(\App\Models\User::class)->create()->id;
         },
         'title'       => $faker->sentence(),
-        'image'       => $image ?: 'http://www.veho.ru/img/photo_not_found.gif',
+        'image'       => 'http://www.veho.ru/img/photo_not_found.gif',
         'body'        => $faker->realText(2000),
     ];
 });
