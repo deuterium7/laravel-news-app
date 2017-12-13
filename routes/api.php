a7fb8b6d7eca8c270c4118793980648b06ca6778
deuterium7 <post0778@gmail.com>
2017-12-13 14:31:06 +0200

    Сделал роуты более читабельным

--- routes/api.php
+++ routes/api.php
@@ -3,30 +3,28 @@
 Route::group(['namespace' => 'API', 'middleware' => 'auth:api'], function () {
     Route::middleware('ban')->group(function () {
         Route::middleware('admin')->group(function () {
-            Route::resource('articles', 'ArticleController', ['except' => ['create', 'edit']]);
+            Route::apiResource('comments', 'CommentController', ['except' => 'index']);
+            Route::apiResource('users', 'UserController', ['except' => 'index']);
+            Route::apiResource('categories', 'CategoryController');
+            Route::apiResource('articles', 'ArticleController');
+
             Route::get('/admin/articles', 'ArticleController@articlesAdmin');
-
-            Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-
-            Route::resource('comments', 'CommentController', ['except' => ['index', 'create', 'edit']]);
             Route::get('/admin/comments', 'CommentController@commentsAdmin');
-
-            Route::resource('users', 'UserController', ['except' => ['index', 'create', 'edit']]);
             Route::get('/admin/users', 'UserController@usersAdmin');
+
             Route::put('/users/{user}/admin', 'UserController@admin');
         });
 
-        Route::get('/articles', 'ArticleController@index');
-        Route::get('/articles/{article}', 'ArticleController@show');
-
-        Route::get('/categories', 'CategoryController@index');
-        Route::get('/categories/{category}', 'CategoryController@show');
-
         Route::get('/comments/{comment}/{from}', 'CommentController@commentsClient');
-        Route::post('/comments', 'CommentController@store');
-        Route::put('/comments/{comment}', 'CommentController@update');
-
+        Route::get('/categories/{category}', 'CategoryController@show');
+        Route::get('/articles/{article}', 'ArticleController@show');
+        Route::get('/categories', 'CategoryController@index');
+        Route::get('/articles', 'ArticleController@index');
         Route::get('/users/{user}', 'UserController@show');
         Route::get('/auth', 'UserController@auth');
+
+        Route::post('/comments', 'CommentController@store');
+
+        Route::put('/comments/{comment}', 'CommentController@update');
     });
 });
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,14 @@
 Route::middleware('guest')->group(function () {
     Route::get('/login/{social}/callback', 'AuthController@socialCallback');
     Route::get('/login/{social}', 'AuthController@socialRedirect');
-
     Route::get('/login', 'AppController@login')->name('login');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', 'AppController@app');
-    Route::get('/locale/{locale}', 'AppController@locale');
-    Route::post('/contact', 'AppController@send');
-
-    Route::get('/logout', 'AuthController@logout');
-
     Route::get('/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+    Route::get('/locale/{locale}', 'AppController@locale');
+    Route::get('/logout', 'AuthController@logout');
+    Route::get('/', 'AppController@app');
+
+    Route::post('/contact', 'AppController@send');
 });
