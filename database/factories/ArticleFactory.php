5bc6edc3fc47ad8bf78538c77b8ae1fc698e378d
deuterium7 <post0778@gmail.com>
2017-12-13 14:52:26 +0200

    Добавил второй вариант записи связующих полей моделей

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -9,9 +9,11 @@ $factory->define(Article::class, function (Faker $faker) {
     return [
         'category_id' => function () {
             return factory(Category::class)->create()->id;
+            // return Category::all()->random()
         },
         'user_id'     => function () {
             return factory(User::class)->create()->id;
+            // return User::all()->random()
         },
         'title' => $faker->sentence(),
         'image' => 'http://b-smart.co.th/wp-content/uploads/2014/07/not-found-300x300.png',
